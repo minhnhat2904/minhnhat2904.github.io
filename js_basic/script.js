225ddefac4f45eb9606ee690aa75fbaa5f1139c6
minhnhat29042000 <phancaominhnhat10@gmail.com>
2021-04-18 23:09:19 +0700

    fixed: validate form input, show error, add button edit & delete to table, require capacity file

--- js_basic/script.js
+++ js_basic/script.js
@@ -7,34 +7,148 @@ function preview_image__add(event) {
 }
 function preview_image__edit(event) {
   let files = event.target.files;
-  console.log(files);
   let blob = URL.createObjectURL(files[0]);
   let a = event.target.parentNode.parentNode;
   a.querySelector(".preview-image__edit").src = blob;
 }
+function checkError(){
+  let count = 0;
+  let majorInput = document.getElementById("select__add").value;
+  let nameInput = document
+    .getElementById("form__input__add")
+    .value.replace(/\s+/g, "");
+  let imageInput = document.getElementById("input-image__add").value;
+  if (nameInput == "") {
+    document.getElementById("name__required").style.display = "block";
+    count++;
+  } else{
+    if (nameInput.length > 10) {
+      document.getElementById("name__required").style.display = "block";
+      document.getElementById("name__required").innerHTML = "Không được nhập quá 10 kí tự";
+      count++;
+    } else{
+        if (nameInput.charAt(0).search(/\d/) == 0) {
+          document.getElementById("name__required").style.display = "block";
+          document.getElementById("name__required").innerHTML = "Tên không được bắt đầu bằng số";
+          count++;
+        } else{
+          document.getElementById("name__required").style.display = "none";
+        }
+      }
+    }
+  if (majorInput == "Mở để chọn chuyên ngành") {
+    document.getElementById("major__required").style.display = "block";
+    count++;
+  } else{
+    document.getElementById("major__required").style.display = "none";
+  }
+  if(imageInput == ""){
+    document.getElementById("image__required").style.display = "block";
+    count++;
+  } else{
+    let fsize = document.querySelector("#input-image__add").files[0].size;
+    let ftype = document.querySelector("#input-image__add").files[0].type;
+    if(fsize > 17000){
+      document.getElementById("image__required").style.display = "block";
+      document.getElementById("image__required").innerHTML = "Dung lượng file vượt quá giới hạn";
+      count++;
+    } else{
+      console.log(ftype);
+      if(ftype != 'image/png' && ftype != 'image/jepg'){
+        document.getElementById("image__required").style.display = "block";
+        document.getElementById("image__required").innerHTML = "File nhập không đúng";
+        count++;
+      }
+      else{
+        document.getElementById("image__required").style.display = "none";
+      }
+    } 
+  }
+
+
+    if(count == 0){
+      return true;
+    } else{
+      return false;
+    }
+}
+function checkErrorEdit(){
+  console.log("abc");
+  let count = 0;
+  let a = document.querySelector(".save").parentNode.parentNode;
+  let majorInput = a.querySelector(".select__input").value;
+  let nameInput = a.querySelector(".name__input").value.replace(/\s+/g, "");
+  let imageInput = a.querySelector(".input-image__edit").value;
+  if (nameInput == "") {
+    document.getElementById("name__required__edit").style.display = "block";
+    count++;
+  } else{
+    if (nameInput.length > 10) {
+      document.getElementById("name__required__edit").style.display = "block";
+      document.getElementById("name__required__edit").innerHTML = "Không được nhập quá 10 kí tự";
+      count++;
+    } else{
+        if (nameInput.charAt(0).search(/\d/) == 0) {
+          document.getElementById("name__required__edit").style.display = "block";
+          document.getElementById("name__required__edit").innerHTML = "Tên không được bắt đầu bằng số";
+          count++;
+        } else{
+          document.getElementById("name__required__edit").style.display = "none";
+        }
+      }
+    }
+  if (majorInput == "Mở để chọn chuyên ngành") {
+    document.getElementById("major__required__edit").style.display = "block";
+    count++;
+  } else{
+    document.getElementById("major__required__edit").style.display = "none";
+  }
+  if(imageInput == ""){
+    document.getElementById("image__required__edit").style.display = "block";
+    count++;
+  } else{
+    let fsize = a.querySelector(".input-image__edit").files[0].size;
+    let ftype = a.querySelector(".input-image__edit").files[0].type;
+    if(fsize > 17000){
+      document.getElementById("image__required__edit").style.display = "block";
+      document.getElementById("image__required__edit").innerHTML = "Dung lượng file vượt quá giới hạn";
+      count++;
+    } else{
+      console.log(ftype);
+      if(ftype != 'image/png' && ftype != 'image/jepg'){
+        document.getElementById("image__required__edit").style.display = "block";
+        document.getElementById("image__required__edit").innerHTML = "File nhập không đúng";
+        count++;
+      }
+      else{
+        document.getElementById("image__required__edit").style.display = "none";
+      }
+    } 
+  }
+  if(count == 0){
+    return true;
+  } else{
+    return false;
+  }
+}
 function showStudent(event) {
   event.preventDefault(); //ngăn cho trang không reload lại
-  let majorInput = document.getElementById("select__add").value;
-  let nameInput = document.getElementById("form__input__add").value.replace(/\s+/g,"");
-  if(nameInput == "" || (nameInput.length > 10) || (nameInput.charAt(0)).search(/\d/) == 0){
-    document.getElementById("name__required").style.display = "block";
-  } else
-    if(majorInput == "Mở để chọn chuyên ngành"){
-      document.getElementById("major__required").style.display = "block";
-    } else{
-      document.getElementById("name__required").style.display = "none";
-      document.getElementById("major__required").style.display = "none";
-      saveToLocalStorage(objStudent());
-      let getStudent = getStudentFromLocalStorage();
-      displayStudent(getStudent);
-    }
-  };
-function displayStudent(getStudent){
+  if(checkError() == true){
+    saveToLocalStorage(objStudent());
+    let getStudent = getStudentFromLocalStorage();
+    displayStudent(getStudent);
+  }
+}
+function displayStudent(getStudent) {
   document.querySelector("tbody").innerHTML = "";
   for (let i = 0; i < getStudent.length; i++) {
     let new_row = `<tr id="${i}">
-        <td>${i+1}</td>
-        <td><input type="text" readonly value="${getStudent[i].name.trim().replace(/\s+/g," ")}" class="name__input"></td>
+        <td>${i + 1}</td>
+        <td><input type="text" readonly value="${getStudent[i].name
+          .trim()
+          .replace(/\s+/g, " ")}" class="name__input">
+          <p id="name__required__edit" style="display: none;color: red;">Name is required</p>
+        </td>
         <td>
             <select name="" class="select__input" disabled>
                 <option selected>${getStudent[i].major}</option>
@@ -43,19 +157,23 @@ function displayStudent(getStudent){
                 <option value="ATTT">ATTT</option>
                 <option value="HTN">HTN</option>
             </select>
+            <p id="major__required__edit" style="display: none;color: red;">Major is required</p>
         </td>
         
         <td>
             <input type="file" accept="image/*" class="input-image__edit" style="display:none">
-            <img src="${getStudent[i].image}" alt="hienthihinhanh" class="preview-image__edit">
+            <img src="${
+              getStudent[i].image
+            }" alt="hienthihinhanh" class="preview-image__edit">
+            <p id="image__required__edit" style="display: none;color: red;">Image is required</p>
         </td>
-        <td>
-          <button onclick="editRow(event)" class="edit" style="display:block">Edit</button>
-          <button onclick="saveRow(event,${i})" style="display:none" class="save">Save</button>
-       </td>
-        <td>
-          <button onclick="deleteRow(${i})" class="delete" style="display:block">Delete</button>
-          <button onclick="cancel(event)" style="display:none" class="cancel">Cancel</button>
+        <td id="edit_delete">
+          <button onclick="editRow(event)" class="edit">Edit</button>
+          <button onclick="deleteRow(${i})" class="delete">Delete</button>
+        </td>
+        <td id="save_cancel">
+          <button onclick="saveRow(event,${i})" class="save">Save</button>
+          <button onclick="cancel(event)" class="cancel">Cancel</button>
         </td>
     </tr>`;
     document.querySelector("tbody").innerHTML += new_row;
@@ -66,13 +184,11 @@ function editRow(event) {
   a.querySelector("input").readOnly = false;
   a.querySelector("select").disabled = false;
   a.querySelector(".input-image__edit").style.display = "block";
-  a.querySelector(".save").style.display = "block";
-  a.querySelector(".cancel").style.display = "block";
-  a.querySelector(".edit").style.display = "none";
-  a.querySelector(".delete").style.display = "none";
+  a.querySelector("#save_cancel").style.display = "flex";
+  a.querySelector("#edit_delete").style.display = "none";
   a.querySelector(".input-image__edit").onchange = preview_image__edit;
 }
-function saveRow(event,id){
+function saveRow(event, id) {
   let a = event.target.parentNode.parentNode;
   let getStudent = getStudentFromLocalStorage();
   let name = a.querySelector(".name__input").value;
@@ -81,28 +197,31 @@ function saveRow(event,id){
   getStudent[id].name = name;
   getStudent[id].major = major;
   getStudent[id].image = image;
-  saveToLocalStorage(getStudent);
-  displayStudent(getStudent);
-  a.querySelector(".input-image__edit").style.display = "none";
-  a.querySelector(".save").style.display = "none";
-  a.querySelector(".cancel").style.display = "none";
-  a.querySelector(".edit").style.display = "block";
-  a.querySelector(".delete").style.display = "block";
+  if(checkErrorEdit() == true){
+    saveToLocalStorage(getStudent);
+    displayStudent(getStudent);
+    a.querySelector(".input-image__edit").style.display = "none";
+    a.querySelector(".save").style.display = "none";
+    a.querySelector(".cancel").style.display = "none";
+    a.querySelector(".edit").style.display = "block";
+    a.querySelector(".delete").style.display = "block";
+  }
+  
 }
-function cancel(event){
+function cancel(event) {
   let getStudent = getStudentFromLocalStorage();
   displayStudent(getStudent);
 }
 function deleteRow(id__delete) {
-    let getStudent = getStudentFromLocalStorage();
-    getStudent.splice(id__delete,1);
-    // localStorage.clear();
-    array = [];
-    saveToLocalStorage(getStudent);
-    getStudent = getStudentFromLocalStorage();
-    displayStudent(getStudent);
+  let getStudent = getStudentFromLocalStorage();
+  getStudent.splice(id__delete, 1);
+  // localStorage.clear();
+  array = [];
+  saveToLocalStorage(getStudent);
+  getStudent = getStudentFromLocalStorage();
+  displayStudent(getStudent);
 }
-function objStudent(){
+function objStudent() {
   let name = document.getElementById("form__input__add").value;
   let chuyen_nganh = document.getElementById("select__add").value;
   let anh_the = document.getElementById("preview-image__add").src;
@@ -112,7 +231,7 @@ function objStudent(){
     image: anh_the,
   };
   let getStudent = getStudentFromLocalStorage();
-  if(getStudent != null){
+  if (getStudent != null) {
     array = getStudent;
   }
   array.push(obj);
@@ -122,17 +241,17 @@ function saveToLocalStorage(array) {
   const setjson = JSON.stringify(array);
   localStorage.setItem("key", setjson);
 }
-function getStudentFromLocalStorage(){
+function getStudentFromLocalStorage() {
   const getStudent = JSON.parse(localStorage.getItem("key"));
   return getStudent;
 }
-window.onload = function(){
+window.onload = function () {
   let getStudent = getStudentFromLocalStorage();
-  if(getStudent != null){
+  if (getStudent != null) {
     array = getStudent;
   }
   displayStudent(getStudent);
-}
+};
 document.querySelector("#form__add__student").onsubmit = showStudent;
 
 document.querySelector("#input-image__add").onchange = preview_image__add;
